be2f16adb5c38a52bc7e4ad82c2b7029a7d84e67
Levon Tumanyan <letumany@c2r4s1.42yerevan.am>
2022-04-10 15:46:35 +0400

    Eskeetit

--- srcs/pipe.c
+++ srcs/pipe.c
@@ -17,3 +17,29 @@ void	fd_closer(t_korn korn)
 		i++;
 	}
 }
+
+/*  
+** 		--- Opening pipes ---
+**		--- Not sure if it's workin ---
+*/
+void	pi_open(t_korn *korn)
+{
+	int		i;
+	int		*fd[2];
+
+	i = 0;
+	while (i < korn->cmd_count - 1)
+	{
+		if (pipe(fd[i]) == -1)
+			perror("Error\nbash: ");
+		if (korn->cmd[i].output == 1)
+			korn->cmd[i].output = fd[i][1];
+		else
+			close(fd[i][1]);
+		if (korn->cmd[i + 1].input == 0)
+			korn->cmd[i + 1].input = fd[i][0];
+		else
+			close(fd[i][0]);
+		++i;
+	}
+}
